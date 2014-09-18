5cadd82141f66e17281851ec6420b841e796f266
Diogo Biazus <diogob@gmail.com>
2014-09-18 18:01:48 -0400

    do not use cache in initializer

--- config/initializers/moip.rb
+++ config/initializers/moip.rb
@@ -1,5 +1,5 @@
 ::MoIP.setup do |config|
-  config.uri = (PaymentEngines.configuration[:moip_uri] rescue nil) || ''
-  config.token = (PaymentEngines.configuration[:moip_token] rescue nil) || ''
-  config.key = (PaymentEngines.configuration[:moip_key] rescue nil) || ''
+  config.uri = (PaymentEngines.configuration.get_without_cache(:moip_uri) rescue nil) || ''
+  config.token = (PaymentEngines.configuration.get_without_cache(:moip_token) rescue nil) || ''
+  config.key = (PaymentEngines.configuration.get_without_cache(:moip_key) rescue nil) || ''
 end
